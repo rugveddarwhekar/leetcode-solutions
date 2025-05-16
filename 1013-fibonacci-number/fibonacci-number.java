35c4bf8774ae26f51e06e9d0405fdac3e648a1e2
Rugved Darwhekar <rugveddarwhekar@gmail.com>
2025-05-15 19:09:29 -0700

    Time: 0 ms (100.00%) | Memory: 40.9 MB (11.18%) - LeetSync

---
+++ 1013-fibonacci-number/fibonacci-number.java
@@ -0,0 +1,19 @@
+class Solution {
+    public int fib(int n) {
+        if (n < 2) return n;
+        int[] dp = new int[2];
+        dp[0] = 0;
+        dp[1] = 1;
+
+        int i = 2;
+        while (i <= n) {
+            int tmp = dp[1];
+            dp[1] = dp[0] + dp[1];
+            dp[0] = tmp;
+            i++;
+        }
+
+        return dp[1];
+    }
+
+}
