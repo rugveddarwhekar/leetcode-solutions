052fdd52d77a6110b3d5b106f596db8f4cc720fc
Rugved Darwhekar <rugveddarwhekar@gmail.com>
2025-06-24 15:44:06 -0700

    Time: 35 ms (42.97%) | Memory: 45.8 MB (30.99%) - LeetSync

--- 1128-remove-all-adjacent-duplicates-in-string/remove-all-adjacent-duplicates-in-string.java
+++ 1128-remove-all-adjacent-duplicates-in-string/remove-all-adjacent-duplicates-in-string.java
@@ -16,4 +16,59 @@ class Solution {
 
         return sb.reverse().toString();
     }
-}
+
+// variant -> remove all dupes from string not only adjecent
+
+public class Solution1 {
+
+    private static class Letter {
+        char val;
+        int freq;
+
+        Letter(char val, int freq) {
+            this.val = val;
+            this.freq = freq;
+        }
+    }
+
+    public String removeAllAdjacentDuplicatesVariant(String s) {
+        if (s == null || s.isEmpty()) {
+            return "";
+        }
+
+        Deque<Letter> stack = new ArrayDeque<>();
+
+        for (char ch : s.toCharArray()) {
+            if (stack.isEmpty()) {
+                stack.addLast(new Letter(ch, 1));
+                continue;
+            }
+
+            if (stack.peekLast().val == ch) {
+                stack.peekLast().freq++;
+                continue;
+            }
+
+            if (stack.peekLast().freq > 1) {
+                stack.removeLast();
+            }
+
+            if (stack.isEmpty() || stack.peekLast().val != ch) {
+                stack.addLast(new Letter(ch, 1));
+            } else {
+                stack.peekLast().freq++;
+            }
+        }
+
+        if (!stack.isEmpty() && stack.peekLast().freq > 1) {
+            stack.removeLast();
+        }
+
+        StringBuilder result = new StringBuilder();
+        for (Letter letter : stack) {
+            result.append(letter.val);
+        }
+
+        return result.toString();
+    }
+}}
