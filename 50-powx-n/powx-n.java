649ea5e6817ed9d381cdba0ad6f364a7a6d1e488
Rugved Darwhekar <rugveddarwhekar@gmail.com>
2025-06-26 09:57:32 -0700

    Time: 0 ms (100.00%) | Memory: 43.4 MB (15.90%) - LeetSync

--- 50-powx-n/powx-n.java
+++ 50-powx-n/powx-n.java
@@ -1,6 +1,6 @@
 class Solution {
     public double myPow(double x, int n) {
-        return rec(x, (long) n);
+        return rec1(x, (long) n);
     }
     public double rec(double x, long n) {
         if (n == 0) return 1;
@@ -20,5 +20,12 @@ class Solution {
             n = n / 2;
         }
         return res;
+    }
+
+    private double rec1(double x, long n) {
+        if (n == 0) return 1;
+        if (n < 0) return 1.0/rec(x, -1 * n);
+        if (n % 2 == 1) return x * rec(x * x, (n-1) / 2);
+        else return rec(x * x, n / 2);
     }
 }
